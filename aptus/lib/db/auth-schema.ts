cfb9d6ccbd3bc9bf7c572988f132021c887e099f
sankalp-kar <programmercoding01234@gmail.com>
2026-08-31 16:33:13 +0530

    issues still not fixed

--- aptus/lib/db/auth-schema.ts
+++ aptus/lib/db/auth-schema.ts
@@ -34,25 +34,35 @@ export const session = pgTable('session', {
     }),
 });
 
+
 export const account = pgTable('account', {
   id: text('id').primaryKey(),
+
   accountId: text('account_id').notNull(),
   providerId: text('provider_id').notNull(),
+
+  issuer: text('issuer').notNull(),
+
   userId: text('user_id')
     .notNull()
     .references(() => user.id, {
       onDelete: 'cascade',
     }),
+
   accessToken: text('access_token'),
   refreshToken: text('refresh_token'),
   idToken: text('id_token'),
+
   accessTokenExpiresAt: timestamp('access_token_expires_at'),
   refreshTokenExpiresAt: timestamp('refresh_token_expires_at'),
+
   scope: text('scope'),
   password: text('password'),
+
   createdAt: timestamp('created_at').defaultNow().notNull(),
   updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
+
 
 export const verification = pgTable('verification', {
   id: text('id').primaryKey(),
