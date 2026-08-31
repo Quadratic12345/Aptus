7ed6535b100113d4bf28d0cb608a489b8ea26618
sankalp-kar <programmercoding01234@gmail.com>
2026-08-31 11:58:01 +0530

    some more things added

--- aptus/lib/db/index.ts
+++ aptus/lib/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import * as schema from './schema';
+import * as authSchema from './auth-schema';
 
 const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql, { schema });
+export const db = drizzle(sql, { schema: { ...schema, ...authSchema } });
